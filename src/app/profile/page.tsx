764ac7700933a37a3da986ee8e4ece682f77ab93
Maliot100X <vejseliminir@gmail.com>
2026-01-29 23:51:59 +0100

    Fix Profile Page - Add Missing Closing Div

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -437,6 +437,7 @@ export default function ProfilePage() {
                         )}
                     </div>
                 </div>
-            </div >
-            );
+            </div>
+        </div>
+    );
 }
