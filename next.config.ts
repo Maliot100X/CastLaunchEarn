60cef292d5c83f60a061f822a86cf5c49760046c
Maliot100X <vejseliminir@gmail.com>
2026-01-30 18:40:21 +0100

    Configure 307 redirect for Farcaster hosted manifest

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019c0ff4-35e6-a9fd-7079-bf402e581696',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
